6aa6c6604991daa0a5cac728fb83f68a37ef2b7b
davinbirdi <davin.birdi@hotmail.ca>
2019-04-27 00:38:47 -0700

    Big Chunk added rgen combinations

--- deckofcards.py
+++ deckofcards.py
@@ -8,28 +8,47 @@ print("Card Problem\n")
 from deckofcardsFunctions import *
 from random import randint
 
-deck1 = createSimpleDeck(5)
-deck2 = createSimpleDeck(5)
 
-deck3 = []
-i = 0
+size1= 3
+size2 = 3
+bigdeck = []
 
 # Dealing Deck1
 print("D:\tDeck1:")
-for i in reversed(range(5)):
-    dealt = randint(0,len(deck1)-1)
-    print(str(deck1[dealt]), end="   ")
-    
-    deck3.append(deck1[dealt])
-    
-    deck1.pop(dealt)
-    
-    print("   ", end="")
-    for x in range(i):
-        print("" + str(deck1[x]), end="")
-    print("")
 
-print("\nDeck3:")
-for i in range(len(deck3)):
-    print(deck3[i], end="")
+# Stack the decks
+# Deal the deck
+for x in range(fact(size1+size2,1)):
+    deck1 = createSimpleDeck(size1)
+    deck2 = createSimpleDeck(size2)
+    deck3 = []
 
+    for i in range(len(deck2)):
+        deck1.append(deck2[i])
+
+    for i in reversed(range(len(deck1))):
+        # len(deck1)-1 to prevent accessing out of range
+        dealt = randint(0, len(deck1)-1)
+        #print(str(deck1[dealt]), end="   ")
+        deck3.append(deck1[dealt])
+        deck1.pop(dealt)
+        
+        #print("   ", end="")
+        #for x in range(i):
+        #    print("" + str(deck1[x]), end="")
+        #print("")
+        
+    print("\nDeck3:")
+    for i in range(len(deck3)):
+        print(deck3[i], end="")
+
+    bigdeck.append(deck3)
+
+
+
+print('\n\nEach Variation')
+
+for x in range(len(bigdeck)):
+    for i in range(len(deck3)):
+        print(bigdeck[x][i], end="")
+    print('')
--- deckofcardsFunctions.py
+++ deckofcardsFunctions.py
@@ -2,9 +2,9 @@
 # Functions:
 # --------------------------------------
 
-# Create Card Deck
-# Inputs: None
-# Outputs: Deck of 52 suited cards
+# Function: Create Card Deck
+# Inputs:   None
+# Outputs:  Deck of 52 suited cards
 
 def createDeck():
     deck = []
@@ -17,9 +17,9 @@ def createDeck():
     
     return deck
 
-# Create a Simpler Deck to work with:
-# Inputs:  num - size of deck
-# Outputs: Deck of size n
+# Function: Create a Simpler Deck to work with:
+# Inputs:   num - size of deck
+# Outputs:  Deck of size n
 def createSimpleDeck(num):
     container = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
     deck = []
@@ -28,3 +28,41 @@ def createSimpleDeck(num):
         deck.append(container[i])
         i = i + 1
     return deck
+
+
+# Function: Factorial
+# Input:    int n
+# Output:   int n!
+def factTR(n,a):
+    if (n == 1):
+        return a
+    return factTR(n-1, n * a)
+
+def fact(n,a):
+    if (n == 1):
+        return a
+    return factTR(n-1, n * a)
+
+
+
+
+'''  Don't want to throw this away just yet
+#Dealing deck1 randomly into deck3
+for i in reversed(range(5)):
+    # len(deck1)-1 to prevent accessing out of range
+    dealt = randint(0, len(deck1)-1)
+    print(str(deck1[dealt]), end="   ")
+    deck3.append(deck1[dealt])
+    deck1.pop(dealt)
+    
+    print("   ", end="")
+    for x in range(i):
+        print("" + str(deck1[x]), end="")
+    print("")
+'''
+'''
+for i in range(len(deck1)):
+    dealt = randint(0, len(deck1)-1)
+    deck3.append(deck1[dealt])
+    deck1.pop(dealt)
+ '''   
